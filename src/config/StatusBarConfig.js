e394f4deab03e788503b809a62cd31e963e3f707
faabiopontes <faabiopontes@gmail.com>
2018-10-31 01:17:37 -0300

    FEA React Native Course - Styling Header and StatusBar

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar } from 'react-native';
+
+StatusBar.setBackgroundColor('#B0C4DE');
+StatusBar.setBarStyle('light-content');
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,15 @@ import { createStackNavigator } from 'react-navigation';
 
 import Main from './pages/main';
 
-export default createStackNavigator({
-  Main
-});
+export default createStackNavigator(
+  {
+    Main
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#B0C4DE'
+      },
+      headerTintColor: '#FFF'
+    }
+  });
